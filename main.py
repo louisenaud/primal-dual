6d35053ff8a59ae3f6e9095f398bbf9859e49537
Louise Naud <louise.naud@gmail.com>
2017-08-16 17:40:59 -0700

    debug - I

--- main.py
+++ main.py
@@ -13,37 +13,6 @@ from scipy.misc import face
 import multiprocessing
 
 
-def nabla(I):
-    """
-    Function to compute the forward gradient of an image
-    :param I: 
-    :return: 
-    """
-    h, w = I.shape
-    G = np.zeros((h, w, 2), I.dtype)
-    G[:, :-1, 0] -= I[:, :-1]
-    G[:, :-1, 0] += I[:, 1:]
-    G[:-1, :, 1] -= I[:-1]
-    G[:-1, :, 1] += I[1:]
-    return G
-
-
-def nablaT(G):
-    """
-    Function to compute the transpose of the forward gradient on an image
-    :param G: 
-    :return: 
-    """
-    h, w = G.shape[:2]
-    I = np.zeros((h, w), G.dtype)
-    # note that we just reversed left and right sides
-    # of each line to obtain the transposed operator
-    I[:, :-1] -= G[:, :-1, 0]
-    I[:, 1: ] += G[:, :-1, 0]
-    I[:-1]    -= G[:-1, :, 1]
-    I[1: ]    += G[:-1, :, 1]
-    return I
-
 def forward_gradient(im):
     """
     Function to compute the forward gradient of the image I.
@@ -59,12 +28,13 @@ def forward_gradient(im):
 
     return gradient
 
+
 def backward_divergence(grad):
     """
     Function to compute the backward divergence.
     Definition in : http://www.ipol.im/pub/art/2014/103/, p208
     
-    :param I: numpy array [NxMx2], array with the same dimensions as the gradient of the image to denoise.
+    :param grad: numpy array [NxMx2], array with the same dimensions as the gradient of the image to denoise.
     :return: numpy array [NxM], backward divergence 
     """
 
@@ -82,55 +52,104 @@ def backward_divergence(grad):
     d_v[-1:, :] = -grad[-2:-1, :, 1]
 
     # Divergence
+    div = np.zeros((h, w), grad.dtype)
     div = d_h + d_v
     return div
 
 
-def anorm(x):
-    '''Calculate L2 norm over the last array dimention'''
-    return np.sqrt((x * x).sum(-1))
+def norm2(x):
+    """
+    L2 norm.
+    :param x: 
+    :return: 
+    """
+    return np.sqrt((x**2).sum(-1))
 
 
-def calc_energy_ROF(X, observation, clambda):
-    Ereg = anorm(nabla(X)).sum()
-    Edata = 0.5 * clambda * ((X - observation) ** 2).sum()
-    return Ereg + Edata
+def norm1(x):
+    """
+    L1 norm.
+    :param x: 
+    :return: 
+    """
+    return np.abs(x).sum(-1)
 
 
-def calc_energy_TVL1(X, observation, clambda):
-    Ereg = anorm(nabla(X)).sum()
-    Edata = clambda * np.abs(X - observation).sum()
-    return Ereg + Edata
+def dual_energy_tvl1(y, im_obs):
+    """
+    Compute the dual energy of TV-L1 problem.
+    :param y: 
+    :param im_obs: numpy array, observed image
+    :return: float, dual energy
+    """
+    nrg = -0.5 * (im_obs - backward_divergence(y))**2
+    nrg = np.sum(nrg)
+    return nrg
 
 
-def project_nd(P, r):
-    '''perform a pixel-wise projection onto R-radius balls'''
-    nP = np.maximum(1.0, anorm(P) / r)
-    return P / nP[..., np.newaxis]
+def dual_energy_rof(y, im_obs):
+    """
+    Compute the dual energy of TV-L1 problem.
+    :param y: 
+    :param im_obs: numpy array, observed image
+    :return: float, dual energy
+    """
+    nrg = -0.5 * (im_obs - backward_divergence(y))**2
+    nrg = nrg.sum()
+    return nrg
 
 
-def shrink_1d(X, F, step):
-    '''pixel-wise scalar srinking'''
-    return X + np.clip(F - X, -step, step)
+def primal_energy_rof(x, img_obs, clambda):
+    """
 
-def solve_ROF(img, clambda, iter_n=101):
-    # setting step sizes and other params
-    L2 = 8.0
-    tau = 0.02
-    sigma = 1.0 / (L2*tau)
-    theta = 1.0
+    :param x: 
+    :param img_obs: 
+    :param clambda: 
+    :return: 
+    """
+    g = forward_gradient(x)
+    nrg = wp * np.maximum(g, 0) + wn * np.maximum(-g, 0)
+    a = np.matmul(np.transpose(x), x)
+    b = np.matmul(np.transpose(img_obs), x)
+    nrg = 0.5* a.sum() + b.sum() + nrg.sum()
+    #energy_reg = norm1(forward_gradient(x)).sum()
+    #energy_data_term = 0.5 * clambda * ((x - img_obs) ** 2).sum()
+    return nrg
 
-    X = img.copy()
-    P = nabla(X)
-    for i in xrange(iter_n):
-        P = project_nd( P + sigma*nabla(X), 1.0 )
-        lt = clambda * tau
-        X1 = (X - tau * nablaT(P) + lt * img) / (1.0 + lt)
-        X = X1 + theta * (X1 - X)
-        if i % 10 == 0:
-            print "%.2f" % calc_energy_ROF(X, img, clambda),
-    print
-    return X
+
+def primal_energy_tvl1(X, observation, clambda):
+    """
+
+    :param X: 
+    :param observation: 
+    :param clambda: 
+    :return: 
+    """
+    energy_reg = norm2(forward_gradient(X)).sum()
+    energy_data_term = clambda * np.abs(X - observation).sum()
+    return energy_reg + energy_data_term
+
+
+def proximal_linf_ball(p, r):
+    """
+    
+    :param p: 
+    :param r: 
+    :return: 
+    """
+    n_p = np.maximum(1.0, norm2(p) / r)
+    return p / n_p[..., np.newaxis]
+
+
+def proximal_l1(x, f, clambda):
+    """
+    
+    :param x: 
+    :param f: 
+    :param clambda: 
+    :return: 
+    """
+    return x + np.clip(f - x, -clambda, clambda)
 
 
 if __name__ == '__main__':
@@ -145,25 +164,65 @@ if __name__ == '__main__':
     ax2.imshow(img_obs)
     plt.show()
 
-    g1 = nabla(img_obs)
     g2 = forward_gradient(img_obs)
-    i1 = nablaT(g1)
     i2 = backward_divergence(g2)
 
-    # some reasonable lambdas
-    lambda_ROF = 1.0
+    norm_l = 8.0
+    max_it = 500
+    theta = 1.0
+    alpha = 0.1
+    w_sig = 10.0
+    L2 = 8.0
+    tau = 0.01
+    sigma = 1.0 / (norm_l * tau)
     lambda_TVL1 = 1.0
+    lambda_rof = 1.5
 
-    print "ROF:",
-    print calc_energy_ROF(img_obs, img_obs, lambda_ROF),
-    print calc_energy_ROF(img_ref, img_obs, lambda_ROF)
-    print "TV-L1:",
-    print calc_energy_TVL1(img_obs, img_obs, lambda_TVL1),
-    print calc_energy_TVL1(img_ref, img_obs, lambda_TVL1)
+    x = -img_obs
+    x_tilde = x
+    h, w = img_ref.shape
+    y = np.zeros((h, w, 2))
+    wn = w_sig * np.random.random((h, w, 2))
+    wp = wn
 
-    img_denoised = solve_ROF(img_obs, 8.0)
+    p_nrg = primal_energy_rof(x, img_obs, lambda_TVL1)
+    print "Primal Energy = ", p_nrg
+    d_nrg = dual_energy_rof(y, img_obs)
+    print "Dual Energy = ", d_nrg
 
+    # Solve ROF
+    primal = np.zeros((max_it,))
+    dual = np.zeros((max_it,))
+    gap = np.zeros((max_it,))
+    primal[0] = p_nrg
+    dual[0] = d_nrg
+    y = forward_gradient(x)
+    for it in range(max_it):
+        # Dual update
+        y = y + sigma * forward_gradient(x_tilde)
+        y = np.maximum(-wn, np.minimum(y, wp))
+        # Primal update
+        x_old = x
+        x = (x - tau * (img_obs - backward_divergence(y))) / (1.0 + tau)
+        # Smoothing
+        x_tilde = x + theta * (x - x_old)
 
+        # Compute energies
+        primal[it] = primal_energy_rof(x_tilde, img_obs, sigma)
+        dual[it] = dual_energy_rof(y, img_obs)
+        gap[it] = primal[it] - dual[it]
+
+    plt.figure()
+    plt.plot(np.asarray(range(max_it)), primal, label="Primal Energy")
+    plt.legend()
+
+    plt.figure()
+    plt.plot(np.asarray(range(max_it)), dual, label="Dual Energy")
+    plt.legend()
+
+    plt.figure()
+    plt.plot(np.asarray(range(max_it)), gap, label="Gap")
+    plt.legend()
 
     # row and column sharing
     f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex='col', sharey='row')
@@ -171,6 +230,7 @@ if __name__ == '__main__':
     ax1.set_title("Reference image")
     ax2.imshow(img_obs)
     ax2.set_title("Observed image")
-    ax3.imshow(img_denoised)
+    ax3.imshow(x)
     ax3.set_title("Denoised image")
     plt.show()
+
